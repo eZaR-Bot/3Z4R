c7a65bbcaf21be8bc3f69305127c7cad94e03929
Sham <thesham@proton.me>
2022-07-17 20:42:20 +0200

    fix: apply suggested changes/fixes

--- ezar/cogs/misc.py
+++ ezar/cogs/misc.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 
 from disnake import CommandInter, Member, User
 from disnake.ext.commands import Cog, slash_command
@@ -18,8 +18,8 @@ class Miscellaneous(Cog):
     async def icon(self, itr: CommandInter):
         """Icon/Profile-Picture-related commands"""
 
-    @icon.sub_command()
-    async def server(self, itr: CommandInter):
+    @icon.sub_command("server")
+    async def icon_server(self, itr: CommandInter):
         """Returns the server icon URL."""
         avatar_embed = Embeb()
         avatar_embed.set_image(url=itr.guild.icon.with_size(1024).url)
@@ -29,11 +29,11 @@ class Miscellaneous(Cog):
         )
         await itr.response.send_message(embed=avatar_embed)
 
-    @icon.sub_command()
-    async def user(
+    @icon.sub_command("user")
+    async def icon_user(
         self,
         itr: CommandInter,
-        user: User = None,
+        user: Optional[User] = None,
         type: Literal["global", "local"] = "local",
     ):
         """Returns a user's avatar.
@@ -43,9 +43,7 @@ class Miscellaneous(Cog):
         user: The user to get the avatar of.
         type: The type of avatar to get."""
         if user is None:
-            user = itr.user
-        elif user in itr.guild.members:
-            user: Member = await itr.guild.getch_member(user.id)
+            user: Member = itr.user
 
         avatar_embed = Embeb(
             color=user.color if user in itr.guild.members else Colors.purple
@@ -55,11 +53,26 @@ class Miscellaneous(Cog):
             avatar_embed.set_author(
                 name=user.name, icon_url=user.avatar.url or user.default_avatar.url
             )
-            await itr.send(embed=avatar_embed)
         elif type == "local":
             avatar_embed.set_author(name=user.name, icon_url=user.display_avatar.url)
             avatar_embed.set_image(url=user.display_avatar.url)
-            await itr.send(embed=avatar_embed)
+
+        return await itr.send(embed=avatar_embed)
+
+    @slash_command()
+    async def info(self, itr: CommandInter):
+        """Information-related commands"""
+
+    @info.sub_command("server")
+    async def info_server(self, itr: CommandInter):
+        """Returns the server information."""
+        info_embed = Embeb(
+            description=itr.guild.description,
+        )
+        info_embed.set_author(name=itr.guild.name, icon_url=itr.guild.icon)
+        info_embed.set_thumbnail(url=itr.guild.icon.url if itr.guild.icon else "")
+        info_embed.add_field(name="Owner", value=f"`{itr.guild.owner}`")
+        await itr.response.send_message(embed=info_embed)
 
 
 def setup(bot: Ezar):
